30551851f28136a4b668db050d47fd675c060d86
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-08-01 20:12:35 -0400

    styles

--- typingGame/src/components/TypingTest.jsx
+++ typingGame/src/components/TypingTest.jsx
@@ -2,11 +2,17 @@ const TypingTest = () => {
     const testParagraph = "This is a typing test"
   
     return (
-      <div className='container'>
+      <div className='container'> 
         <div className='test'>
           {testParagraph.split("").map((char, index) => (
             <span className='character' key={index}>{char}</span>
           ))}
+        </div>
+        <div className="testFooter">
+          <p>Mistakes: </p>
+          <p>WPM: </p>
+          <p>Difficulty: </p>
+          <button className="btn">Restart</button>
         </div>
       </div>
     )
